09b95f4901aa5daa544adb72c232c2173ba77bfc
CYBER-MRINAL <hacker214230@duck.com>
2025-07-29 21:19:56 +0530

    ALL FIXES

--- level12.sh
+++ level12.sh
@@ -10,3 +10,42 @@ echo
 echo -e "\e[96maHR0cHM6Ly9jeWJlci1tcmluYWwuZ2l0aHViLmlvL29tc3dhc3RyYQ==\e[0m"
 echo
 echo -e "\e[90m(Decode the string above using base64 to reach your destiny.)\e[0m"
+
+#
+#                      <<---TARGET-LIST--->>
+#                           (2023-2024)
+#
+#                <<---(FAKE-VIDEO-SPREAD-2023)--->>
+#
+#(Software - "DeepSound", Psswd - "MAKE IT REAL", Stored - "SANDISK")
+#
+#    1.  - ANITA MONDAL -> 7001056149
+#   2.  - ANKAN MONDAL -> 9593133872
+#    3.  - ARPITA MONDAL -> 9093150607
+#    4.  - ARPITA BF -> 9735229210
+#    5.  - ASIS (PROTAP)(SAKTIPUR) -> 7384833560
+#    6.  - BEDESRUTI -> 1. 9564736855 / 2. 91533422288
+#    7.  - BISHAL (SOUMI BF) -> 9382690812
+#    8.  - DIP -> 1. 8478800960 / 2. 9091900512 
+#    9.  - MRINMOY -> 8167489348
+#    10. - PROBIR (KHM EX) -> 7550939199
+#    11. - PRITOM -> 1. 9547154053 / 2. 8906042265
+#    12. - PROTAP (SLAP IN SAKTI) -> 8768829562
+#    13. - RAGHAB -> 1. 8370928281 / 2. 8900494844
+#    14. - RIMI GHOSH -> 9734542744
+#    15. - ROHAN SAHA -> 6295662044
+#    16. - RUDRO MONDAL -> 1. 7001448135
+#    17. - RUPONKOR -> 6294500214
+#    18. - SAYANTONI MONDAL -> 1. 8389808304 / 2. 7029811126
+#   19. - SAYANTONI R MA -> 9641164394
+#    20. - SOHEL -> 8101448668
+#    21. - SOUMI BISWAS -> 7908818417
+#    22. - SRIJA R DADA -> 6294290135
+#    23. - SRIJA R BABA -> 7797122722
+#    24. - SRIJA R MA -> 9593861825
+#    25. - SRIJA R PICI -> 9735705095
+#    26. - SUPRIYO -> 8515823120
+#    28. - SUSMITA -> 1. 9832798096 / 2. 7001878328
+#    29. - TRIDP PANDAY -> 6294735431
+#    30. - TUBAI DA (KHM) -> 9308580924
+#    31. - TRINA SAMADDER -> 7865088944
