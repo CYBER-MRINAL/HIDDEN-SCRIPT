c5aabfc780b1480bf3515789cc291c42f9e0af85
CYBER-MRINAL <hacker214230@duck.com>
2025-07-30 23:19:03 +0530

    FIXES ADDED

--- level12.sh
+++ level12.sh
@@ -7,6 +7,7 @@ echo
 echo -e "\e[92m[✓] You've completed all 12 levels!\e[0m"
 echo -e "\e[93m[→] FINAL PAYLOAD UNLOCKED:\e[0m"
 echo
+echo "(^_^) JUST DECODE IT THIS IS LOOKS LIKE B_SE_4"
 echo -e "\e[96maHR0cHM6Ly9jeWJlci1tcmluYWwuZ2l0aHViLmlvL29tc3dhc3RyYQ==\e[0m"
 echo
 echo -e "\e[90m(Decode the string above using base64 to reach your destiny.)\e[0m"
